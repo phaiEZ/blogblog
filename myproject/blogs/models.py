709556fda01ce050eec4b0c3bb1bde8e7df8256e
plmssr <92752291+plmssr@users.noreply.github.com>
2021-12-20 02:39:04 +0700

    up

--- myproject/blogs/models.py
+++ myproject/blogs/models.py
@@ -1,16 +1,49 @@
 from django.db import models
 from category.models import Category
+
 # Create your models here.
 class Blogs(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     content = models.TextField()
-    #link Category from category
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)#can't delete if stil have category
+    # link Category from category
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE
+    )  # can't delete if stil have category
     writer = models.CharField(max_length=255)
     views = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="blogImages",blank=True)#blogImages is folder
+    image = models.ImageField(
+        upload_to="blogImages", blank=True
+    )  # blogImages is folder
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name#เอาไปแสดงผลในหน้า admin
+        return self.name  # เอาไปแสดงผลในหน้า admin
+
+
+class Queue:
+    def __init__(self):
+        self.data = []
+
+    def add(self, value):
+        self.data.append(value)
+
+    def pop(self):
+        return self.data.pop(0)
+
+    def range(self):
+        return len(self.data)
+
+
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def add(self, value):
+        self.data.append(value)
+
+    def pop(self):
+        return self.data.pop(-1)
+
+    def range(self):
+        return len(self.data)
