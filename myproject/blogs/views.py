e719ae927f6b5c496bd5ee51e61af4d1399a3000
plmssr <92752291+plmssr@users.noreply.github.com>
2021-12-20 02:36:48 +0700

    update datastruc

--- myproject/blogs/views.py
+++ myproject/blogs/views.py
@@ -1,24 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from category.models import Category
-from .models import Blogs
+from .models import Blogs, Queue, Stack
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
 
 
 def index(request):
-    ################################################################
-    # pleum
-
     categories = Category.objects.all()
     blogs = Blogs.objects.all()
-    lastest = Blogs.objects.all().order_by("-pk")[:4]
-    # most views
-    popular = Blogs.objects.all().order_by("-views")[:3]
-    # least views
-    suggestion = Blogs.objects.all().order_by("views")[:3]
+    print(blogs[0].views)
+    lastest = Blogs()
+    popular = []
+    lastest = []
+    suggestion = []
+    # บทความแนะนำ ใช้ Queue
+    que = Queue()
+    for i in blogs:
+        que.add(i)
+    mergeSort(que.data)  # ใช้ mergesort เรียงตามยอดview จาก น้อย->มาก
+    for i in range(3):
+        suggestion.append(que.pop())
 
+    # บทความล่าสุด และ บทความยอดนิยม ใช้ stack
+    stack = Stack()
+
+    for i in blogs:
+        stack.add(i)
+
+    for i in range(2):
+        lastest.append(stack.pop())
+
+    quick_sort(
+        0, stack.range() - 1, stack.data
+    )  # ใช้ quicksort เรียงตามยอดview จาก น้อย->มาก
+
+    for i in range(3):
+        popular.append(stack.pop())
+
+    # most views
+    # popular = Blogs.objects.all().order_by('-views')[:3]
+    # least views
+    # suggestion = Blogs.objects.all().order_by('views')[:3]
     # pagination
     paginator = Paginator(blogs, 2)
     try:
@@ -45,7 +69,11 @@ def index(request):
 
 
 def blogDetail(request, id):
-    singleBlog = Blogs.objects.get(id=id)
+    # singleBlog = Blogs.objects.get(id=id)
+    blogs = Blogs.objects.all()
+    singleBlog = binarySearch(
+        blogs, 0, len(blogs) - 1, id
+    )  # ใช้ binarysearch หาบทความที่มีid ตรงกัน
     singleBlog.views = singleBlog.views + 1
     singleBlog.save()
     categories = Category.objects.all()
@@ -64,7 +92,12 @@ def blogDetail(request, id):
 
 
 def searchCategory(request, cat_id):
-    blogs = Blogs.objects.filter(category_id=cat_id)
+    # blogs = Blogs.objects.filter(category_id=cat_id)
+    allblogs = Blogs.objects.all()
+    blogs = []
+    for i in allblogs:
+        if i.category_id == cat_id:
+            blogs.append(i)
     categories = Category.objects.all()
     popular = Blogs.objects.all().order_by("-views")[:3]
     suggestion = Blogs.objects.all().order_by("views")[:3]
@@ -84,7 +117,12 @@ def searchCategory(request, cat_id):
 
 
 def searchWriter(request, writer):
-    blogs = Blogs.objects.filter(writer=writer)
+    allBlogs = Blogs.objects.all()
+    blogs = []
+    print(allBlogs[0])
+    for i in allBlogs:  # ใช้ linearsearch หาบทความตามชื่อผู้เขียน
+        if i.writer == writer:
+            blogs.append(i)
     categories = Category.objects.all()
     popular = Blogs.objects.all().order_by("-views")[:3]
     suggestion = Blogs.objects.all().order_by("views")[:3]
@@ -99,3 +137,100 @@ def searchWriter(request, writer):
             "writer": writer,
         },
     )
+
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key.views > arr[j].views:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+
+        L = arr[:mid]
+
+        R = arr[mid:]
+
+        mergeSort(L)
+
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i].views < R[j].views:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+def partition(start, end, array):
+
+    pivot_index = start
+    pivot = array[pivot_index]
+
+    while start < end:
+
+        while start < len(array) and array[start].views <= pivot.views:
+            start += 1
+
+        while array[end].views > pivot.views:
+            end -= 1
+
+        if start < end:
+            array[start], array[end] = array[end], array[start]
+
+    array[end], array[pivot_index] = array[pivot_index], array[end]
+
+    return end
+
+
+def quick_sort(start, end, array):
+
+    if start < end:
+
+        p = partition(start, end, array)
+
+        quick_sort(start, p - 1, array)
+        quick_sort(p + 1, end, array)
+
+
+def binarySearch(arr, l, r, x):
+
+    # Check base case
+    if r >= l:
+
+        mid = l + (r - l) // 2
+
+        if arr[mid].id == x:
+            return arr[mid]
+
+        elif arr[mid].id > x:
+            return binarySearch(arr, l, mid - 1, x)
+
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+
+    else:
+
+        return -1
