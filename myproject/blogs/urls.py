d5f57838d0b695808cfafd96b1b3b36fcb6a8490
plmssr <92752291+plmssr@users.noreply.github.com>
2021-12-21 00:41:18 +0700

    final 2

--- myproject/blogs/urls.py
+++ myproject/blogs/urls.py
@@ -3,8 +3,8 @@ from .views import index, blogDetail, searchCategory, searchName, searchWriter
 
 urlpatterns = [
     path("", index),
-    path("blog/<int:id>", blogDetail, name="blogDetail"),
-    path("blog/category/<int:cat_id>", searchCategory, name="searchCategory"),
-    path("blog/writer/<str:writer>", searchWriter, name="searchWriter"),
+    path("<int:id>", blogDetail, name="blogDetail"),
+    path("category/<int:cat_id>", searchCategory, name="searchCategory"),
+    path("writer/<str:writer>", searchWriter, name="searchWriter"),
     path("searchName", searchName, name="searchName"),
 ]
